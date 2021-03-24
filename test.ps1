7434c5e269b9b82d71bf349e7b3687641aa56cb6
Jos Koelewijn <joskw@live.com>
2021-03-24 20:52:29 +0100

    fix bug in test code

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 $body = @{
-    code="gps * | %{$_.fullname}"
+    code="gps * | %{`$_.fullname}"
 } | convertto-json
 
 Invoke-RestMethod -Uri "http://localhost:7071/api/SyntaxAnalyzer" -Method Post -Body $body
