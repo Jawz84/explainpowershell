0ad0100297fe24f1dd320928c076e7f0ac2d120a
Jos Koelewijn <joskw@live.com>
2021-03-29 22:37:21 +0200

    refactor for synopsis and full cmdname

--- explainpowershell.analysisservice/SyntaxAnalyzer.cs
+++ explainpowershell.analysisservice/SyntaxAnalyzer.cs
@@ -14,11 +14,21 @@ using System.Text;
 using System.Net.Http;
 
 using explainpowershell.models;
+using System.Linq;
 
 namespace ExplainPowershell.SyntaxAnalyzer
 {
     public class SyntaxAnalyzer
     {
+        private string extent;
+        private int offSet = 0;
+        
+        private static readonly PowerShell powerShell = PowerShell.Create();
+        private CommandInvocationIntrinsics invokeCommand = powerShell.Runspace.SessionStateProxy.InvokeCommand;
+        private Dictionary<String, String> AliasToCmdletDictionary;
+
+        public CommandInvocationIntrinsics InvokeCommand { get => invokeCommand; set => invokeCommand = value; }
+
         [FunctionName("SyntaxAnalyzer")]
         public async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
@@ -33,29 +43,9 @@ namespace ExplainPowershell.SyntaxAnalyzer
             ScriptBlock sb = ScriptBlock.Create(code);
             var ast = sb.Ast;
 
-            IEnumerable<Ast> foundCommandAsts = ast.FindAll(ast => ast is CommandAst, true);
-            List<Command> resolvedCmds = new List<Command>();
-            foreach (CommandAst cmd in foundCommandAsts)
-            {
+            extent = ast.Extent.ToString();
 
-                string cmdName = cmd.GetCommandName();
-                if (String.IsNullOrEmpty(cmdName))
-                {
-                    continue;
-                }
-
-                log.LogInformation(cmdName);
-
-                string resolvedCmd = ResolveCmd(cmdName);
-
-                resolvedCmds.Add(new Command() { CommandName = resolvedCmd });
-
-                log.LogInformation(resolvedCmd);
-            }
-
-            string ext = ast.Extent.ToString();
-
-            if (string.IsNullOrEmpty(ext))
+            if (string.IsNullOrEmpty(extent))
             {
                 log.LogError("That didn't go as planned");
                 return new HttpResponseMessage(HttpStatusCode.BadRequest) 
@@ -66,7 +56,39 @@ namespace ExplainPowershell.SyntaxAnalyzer
                 };
             }
 
-            var json = JsonConvert.SerializeObject(resolvedCmds, Formatting.Indented);
+            IEnumerable<Ast> foundCommandAsts = ast.FindAll(ast => ast is CommandAst, true);
+            var explanations = new List<Explanation>();
+
+            foreach (CommandAst cmd in foundCommandAsts)
+            {
+                string resolvedCmd = ResolveCmd(cmd.GetCommandName());
+                if (string.IsNullOrEmpty(resolvedCmd))
+                {
+                    continue;
+                }
+
+                ExpandAliasesInExtent(cmd, resolvedCmd);
+
+                var synopsis = powerShell
+                    .AddScript($"Get-Help {resolvedCmd} | Select-Object -ExpandProperty Synopsis")
+                    .Invoke()
+                    .FirstOrDefault()
+                    .ToString();
+
+                log.LogInformation(resolvedCmd);
+                log.LogInformation(synopsis);
+
+                explanations.Add(
+                    new Explanation() { 
+                        OriginalExtent = cmd.Extent.Text,
+                        CommandName = resolvedCmd,
+                        Synopsis = synopsis
+                    });
+            }
+
+            log.LogInformation("expanded: '{extent}'",  extent);
+
+            var json = JsonConvert.SerializeObject(explanations, Formatting.Indented);
 
             return new HttpResponseMessage(HttpStatusCode.OK)
             {
@@ -75,11 +97,22 @@ namespace ExplainPowershell.SyntaxAnalyzer
 
         }
 
+        private void ExpandAliasesInExtent(CommandAst cmd, string resolvedCmd)
+        {
+            int start = offSet + cmd.Extent.StartOffset;
+            int length = offSet + cmd.CommandElements[0].Extent.EndOffset - start;
+            extent = extent
+                .Remove(start, length)
+                .Insert(start, resolvedCmd);
+
+            offSet = offSet + resolvedCmd.Length - length;
+        }
+
         private string ResolveCmd(string cmdName)
         {
             if (AliasToCmdletDictionary == null)
             {
-                Initialize();
+                InitializeAliasDictionary();
             }
 
             if (AliasToCmdletDictionary.ContainsKey(cmdName))
@@ -87,21 +120,14 @@ namespace ExplainPowershell.SyntaxAnalyzer
                 return AliasToCmdletDictionary[cmdName];
             }
 
-            return String.Empty;
+            return InvokeCommand.GetCommandName(cmdName,false,false).FirstOrDefault();
         }
 
-        private CommandInvocationIntrinsics invokeCommand;
-        private Dictionary<String, String> AliasToCmdletDictionary;
-
-        public void Initialize()
+        public void InitializeAliasDictionary()
         {
-            var ps = PowerShell.Create();
-
-            this.invokeCommand = ps.Runspace.SessionStateProxy.InvokeCommand;
-
             AliasToCmdletDictionary = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase);
 
-            IEnumerable<CommandInfo> aliases = this.invokeCommand.GetCommands("*", CommandTypes.Alias, true);
+            IEnumerable<CommandInfo> aliases = InvokeCommand.GetCommands("*", CommandTypes.Alias, true);
 
             foreach (AliasInfo aliasInfo in aliases)
             {
