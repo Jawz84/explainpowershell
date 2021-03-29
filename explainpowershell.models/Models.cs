f92ca50af0efe6727e0a1975d2ee00ee4df47056
Jos Koelewijn <joskw@live.com>
2021-03-29 22:36:40 +0200

    add explanation model

--- explainpowershell.models/Models.cs
+++ explainpowershell.models/Models.cs
@@ -11,4 +11,11 @@ namespace explainpowershell.models
     {
         public string PowershellCode { get; set; }
     }
+
+    public class Explanation
+    {
+        public string OriginalExtent { get;set; }
+        public string CommandName { get;set; }
+        public string Synopsis { get;set; }
+    }
 }
