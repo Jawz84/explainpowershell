4ab22a6c079f3f2613ea7afccb5fa1121cab6d31
Jos Koelewijn <joskw@live.com>
2021-04-03 19:15:12 +0200

    suppress az upload output json

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@ $source = ".\explainpowershell.frontend\bin\Release\net5.0\publish\wwwroot\"
 dotnet clean -v m &&
     dotnet restore -v m &&
     dotnet publish --no-restore &&
-    az storage blob upload-batch --destination `$web --account-name storageexplainpowershell --destination-path / --source $source --account-key $key &&
+    az storage blob upload-batch --destination `$web --account-name storageexplainpowershell --destination-path / --source $source --account-key $key --output none &&
     Push-Location .\explainpowershell.analysisservice && 
     func azure functionapp publish explainpowershellsyntaxanalyzer --no-build &&
     Pop-Location
