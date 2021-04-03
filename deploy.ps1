1462eb374d92d26704444359c9ae0e96ff41dc8d
Jos Koelewijn <joskw@live.com>
2021-04-03 19:32:24 +0200

    make deploy script also remove previous blob content

--- deploy.ps1
+++ deploy.ps1
@@ -3,6 +3,7 @@ $source = ".\explainpowershell.frontend\bin\Release\net5.0\publish\wwwroot\"
 dotnet clean -v m &&
     dotnet restore -v m &&
     dotnet publish --no-restore &&
+    az storage blob delete-batch --source `$web --account-name storageexplainpowershell --account-key $key --output none &&
     az storage blob upload-batch --destination `$web --account-name storageexplainpowershell --destination-path / --source $source --account-key $key --output none &&
     Push-Location .\explainpowershell.analysisservice && 
     func azure functionapp publish explainpowershellsyntaxanalyzer &&
