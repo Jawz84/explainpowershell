e3d5a82711507a16490b3cfc7feee3137301c517
Jos Koelewijn <joskw@live.com>
2021-04-05 21:27:18 +0200

    Add rudimentary help scraper -> table

---
+++ explainpowershell.helpcollector/explainpowershell.helpcollector.ps1
@@ -0,0 +1,89 @@
+. .\classes.ps1
+
+# Update-Help -Force -ErrorAction SilentlyContinue
+$cmds = New-Object -TypeName 'System.Collections.Generic.Dictionary[[string],[HelpData]]'
+
+Get-Module 
+| Where-Object name -NotLike az* #for testing
+| Sort-Object -Unique -Property Name
+| ForEach-Object {
+    $moduleProjectUri = $_.ProjectUri
+    Get-Command -Module $_.name | ForEach-Object {
+        $cmds.Add(
+            $_.Name,
+            [HelpData]@{
+                ModuleName        = $_.ModuleName
+                CommandName       = $_.Name
+                RawCommandInfo    = $_
+                Syntax            = (Get-Command $_.Name -Syntax).trim()
+                DocumentationLink = $moduleProjectUri
+            }
+        )
+    }
+} 
+
+function Get-SynopsisFromUri ($uri) {
+    try {
+        $html = Invoke-RestMethod $uri
+        ($html.Trim().Split("`n")
+            | Select-String 'class="summary">' -Context 1
+            | Select-Object -ExpandProperty context
+            | Select-Object -ExpandProperty postcontext
+        ).Trim() -replace '<p>|</p>', ''
+    }
+    catch {
+        return
+    }
+}
+
+foreach ($cmd in $cmds.Keys) {
+    $help = Get-Help $cmd
+    $relatedLinks = $help.relatedLinks.navigationLink.where{ $_.uri -match '^http' }.uri
+    if ($null -eq $relatedLinks) {
+        $relatedLinks = $cmds[$cmd].RawCommandInfo.HelpUri
+    }
+
+    $synopsis = $help.synopsis.trim()
+
+    if ($synopsis.startswith($cmd)) {
+        $uri = $relatedLinks | Select-Object -First 1
+        if (-not ($synopsis = Get-SynopsisFromUri $uri)) {
+            $synopsis = 'Not found'
+        }
+    }
+
+    $parameterData = $help.parameters.parameter | ForEach-Object {
+        [RawHelpParameterData]@{
+            aliases          = $_.aliases
+            defaultValue     = $_.defaultValue
+            Description      = $_.Description.text
+            name             = $_.name
+            parameterSetName = $_.parameterSetName
+            parameterValue   = $_.parameterValue
+            pipelineInput    = $_.pipelineInput
+            position         = $_.position
+            required         = $_.required
+            typeName         = $_.typeName
+        }
+    }
+
+    $link = $relatedLinks | Select-Object -First 1
+
+    if (-not [string]::IsNullOrEmpty($link)) {
+        $cmds[$cmd].DocumentationLink = $link
+    }
+    $cmds[$cmd].Synopsis = $synopsis
+    $cmds[$cmd].Parameters = $cmds[$cmd].RawCommandInfo.Parameters.Keys
+    $cmds[$cmd].RawCmdletHelp = [RawHelpData]@{
+        Aliases      = $help.Aliases
+        Description  = $help.Description
+        InputTypes   = [PSTypeName[]]$help.InputTypes.inputType.type.name
+        Parameters   = $parameterData
+        RelatedLinks = $relatedLinks
+        ReturnValues = [PSTypeName[]]$help.ReturnValues.returnValue.type.name
+        Synopsis     = $help.synopsis.trim()
+        Syntax       = $help.syntax
+    }
+}
+
+$cmds[$($cmds.Keys)] 
---
+++ explainpowershell.helpcollector/explainpowershell.helpwriter.ps1
@@ -0,0 +1,32 @@
+. .\classes.ps1
+
+$storageAccountName = 'storageexplainpowershell'
+$tableName = 'HelpData'
+$sasToken = (Get-Content .\sastoken.user).trim()
+$partitionKey = 'CommandHelp'
+$storageCtx = New-AzStorageContext -StorageAccountName $storageAccountName -SasToken $sasToken
+
+if ($null -eq ($table = Get-AzStorageTable -Context $storageCtx -Name $tableName -ErrorAction SilentlyContinue)) {
+    $table = New-AzStorageTable -Context $storageCtx -Name $tableName
+}
+
+#$commandHelp = .\explainpowershell.helpcollector.ps1
+
+foreach ($help in $commandHelp) {
+    $hlp = $help -As [HelpData]
+    Add-AzTableRow -Table $table.CloudTable -PartitionKey $partitionKey -RowKey $hlp.CommandName -property @{
+        ModuleName        = $hlp.ModuleName
+        Synopsis          = $hlp.Synopsis
+        Syntax            = $hlp.Syntax
+       # Parameters        = $hlp.Parameters.Name
+        DocumentationLink = $hlp.DocumentationLink
+        # RawCmdletHelp_aliases     = $hlp.RawCmdletHelp.Aliases
+        # RawCmdletHelp_description = $hlp.RawCmdletHelp.Description
+        # RawCmdletHelp_InputTypes = $hlp.RawCmdletHelp.InputTypes.Name
+        # rawcmdlethelp_ = $hlp.rawcmdlethelp.Parameters
+        # RawCommandInfo    = $hlp.RawCommandInfo
+    } | Out-Null
+}
+
+Read-Host "press enter to delete table $tableName"
+Remove-AzStorageTable -Name $tableName -Context $storageCtx
