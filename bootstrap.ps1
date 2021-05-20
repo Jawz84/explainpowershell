17b34806de9387b424a1733dbbb46ebe2a826f98
Jos Koelewijn <joskw@live.com>
2021-05-20 18:46:30 +0000

    add Force param to bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,11 @@
+[CmdletBinding()]
+param(
+    [Switch]$Force
+)
+
 if ($IsLinux && $env:DOTNET_RUNNING_IN_CONTAINER) {
     Write-Host -ForegroundColor Green "We are running in a container, make sure we have permissions on all folders in the repo, to be able to build and run the application."
-    $testOwnershipAndPermissions = ls -l $PSScriptRoot | Select-String bootstrap -Raw 
+    $testOwnershipAndPermissions = ls -l $PSScriptRoot | Select-String bootstrap -Raw
 
     if ($testOwnershipAndPermissions | Select-String root) {
         Write-Host "Making 'vscode' owner of all files in '$PSScriptRoot/' (recursive)."
@@ -27,7 +32,7 @@ foreach ($module in $modules) {
 Import-Module Posh-Git
 
 $commandsToAddToProfile = @(
-    'Import-Module Posh-Git', 
+    'Import-Module Posh-Git',
     'Set-PSReadLineOption -EditMode Windows'
 )
 
@@ -37,19 +42,19 @@ if ( !(Test-Path -path $profile.CurrentUserAllHosts) ) {
 
 $profileContents = Get-Content -path $profile.CurrentUserAllHosts
 if ($null -eq $profileContents -or
-    $profileContents.split("`n") -notcontains $commandsToAddToProfile[0]) 
+    $profileContents.split("`n") -notcontains $commandsToAddToProfile[0])
 {
     Write-Host -ForegroundColor Green "Add settings to PowerShell profile"
     Add-Content -Path $profile.CurrentUserAllHosts -Value $commandsToAddToProfile
-    # Add copy profile to VSCode profile too: Microsoft.VSCode_profile.ps1
-    Get-Content -Path $profile.CurrentUserAllHosts 
-        | Set-Content -Path ($profile.CurrentUserAllHosts 
-        | Split-Path -Parent 
+    # Copy profile contents to VSCode profile too: Microsoft.VSCode_profile.ps1
+    Get-Content -Path $profile.CurrentUserAllHosts
+        | Set-Content -Path ($profile.CurrentUserAllHosts
+        | Split-Path -Parent
         | Join-Path -ChildPath 'Microsoft.VSCode_profile.ps1') -Force
 }
 
 Write-Host -ForegroundColor Green "Fill local database with help data.."
-& $PSScriptRoot\explainpowershell.helpcollector\explainpowershell.helpwriter.ps1
+& $PSScriptRoot\explainpowershell.helpcollector\explainpowershell.helpwriter.ps1 -Force:$Force
 
 Write-host -ForegroundColor Green "Running tests to see if everything works"
 & $PSScriptRoot/Tests/launch.ps1
