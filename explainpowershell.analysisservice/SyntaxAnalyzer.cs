2c98804a3f86e4af6b66fd58e96c7c653048a8fa
Jos Koelewijn <joskw@live.com>
2021-06-06 19:47:44 +0000

    remove unused usings

--- explainpowershell.analysisservice/SyntaxAnalyzer.cs
+++ explainpowershell.analysisservice/SyntaxAnalyzer.cs
@@ -5,16 +5,13 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Management.Automation;
 using System.Management.Automation.Language;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 using explainpowershell.models;
 
