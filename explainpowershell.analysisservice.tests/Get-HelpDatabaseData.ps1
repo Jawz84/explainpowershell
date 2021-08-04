0fdb2e1c800e20ff3bafb5b2ccd5e59669148237
Jos Koelewijn <joskw@live.com>
2021-08-04 14:50:54 +0000

    refactor out new-sastoken function

--- explainpowershell.analysisservice.tests/Get-HelpDatabaseData.ps1
+++ explainpowershell.analysisservice.tests/Get-HelpDatabaseData.ps1
@@ -23,28 +23,9 @@ function Get-HelpDatabaseData {
     $tableName = 'HelpData'
     $partitionKey = 'CommandHelp'
 
-    function New-SasToken {
-        param(
-            $ResourceGroupName,
-            $StorageAccountName
-        )
-
-        $context = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName).context
-
-        $sasSplat = @{
-            Service = 'Table'
-            ResourceType = 'Service', 'Container', 'Object'
-            Permission = 'racwdlup' # https://docs.microsoft.com/en-us/powershell/module/az.storage/new-azstorageaccountsastoken
-            StartTime  = (Get-Date)
-            ExpiryTime = (Get-Date).AddMinutes(30)
-            Context    = $context
-        }
-
-        return New-AzStorageAccountSASToken @sasSplat
-    }
-
     if ($IsProduction) {
         Get-AzContext
+        . ../explainpowershell.helpcollector/New-SasToken.ps1
         $sasToken = New-SasToken -ResourceGroupName $ResourceGroupName -StorageAccountName $storageAccountName
         $storageCtx = New-AzStorageContext -StorageAccountName $storageAccountName -SasToken $sasToken
     }
