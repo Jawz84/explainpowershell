f9c84e74edd3ac53b577af177cada87f8146ab5c
Jos Koelewijn <joskw@live.com>
2021-08-15 11:23:26 +0000

    add new properties to HelpEntity

--- explainpowershell.models/Models.cs
+++ explainpowershell.models/Models.cs
@@ -41,11 +41,21 @@ namespace explainpowershell.models
 
     public class HelpEntity : TableEntity
     {
+        public string Aliases { get; set; }
+        public string CommandName { get; set; }
+        public string DefaultParameterSet { get; set; }
+        public string Description { get; set; }
         public string DocumentationLink { get; set; }
+        public string InputTypes { get; set; }
+        public string ModuleName { get; set; }
+        public string ModuleProjectUri { get; set; }
+        public string ModuleVersion { get; set; }
+        public string Parameters { get; set; }
+        public string ParameterSetNames { get; set; }
+        public string RelatedLinks { get; set; }
+        public string ReturnValues { get; set; }
         public string Synopsis { get; set; }
         public string Syntax { get; set; }
-        public string ModuleName { get; set; }
-        public string CommandName { get; set; }
     }
 
     public class HelpMetaData
