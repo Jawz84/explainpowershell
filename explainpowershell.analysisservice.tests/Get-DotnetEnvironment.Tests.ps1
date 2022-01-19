2a21e75d72b7207b4705d5fd2a7845071246c183
joskw@live.com <joskw@live.com>
2022-01-19 20:17:38 +0000

    improve tests for dotnet

--- explainpowershell.analysisservice.tests/Get-DotnetEnvironment.Tests.ps1
+++ explainpowershell.analysisservice.tests/Get-DotnetEnvironment.Tests.ps1
@@ -1,22 +1,25 @@
+Push-Location $PSScriptRoot/.. # this line does not work as expected when placed in the BeforeAll block.
+
 BeforeAll {
-    Push-Location $PSScriptRoot/..
+    $requiredDotnetVersions = Get-ChildItem *.csproj -Recurse -Depth 1 | ForEach-Object {
+        [version](
+            [xml](Get-Content $_)
+        ).
+            Project.
+            PropertyGroup.
+            TargetFramework.
+            Replace('net','')
+    } | Sort-Object -Unique
 }
 
 Describe "prerequisites" {
     It "has the right dotnet sdk installed" {
-        $requiredDotnetVersions = Get-ChildItem *.csproj -Recurse -Depth 1 | ForEach-Object {
-            [version](
-                [xml](Get-Content $_)
-            ).
-                Project.
-                PropertyGroup.
-                TargetFramework.
-                Replace('net','')
-        } | Sort-Object -Unique
+        $latestAvailableDotnetVersion = dotnet --list-sdks
+        | ForEach-Object { [version]$_.split(' ')[0] }
+        | Sort-Object
+        | Select-Object -Last 1
 
-        $latestAvailableDotnetVersion = dotnet --list-sdks | ForEach-Object { [version]$_.split(' ')[0] } | Sort-Object | Select-Object -Last 1
-
-        foreach ($version in $requiredDotnetVersions) {
+        foreach ($version in $script:requiredDotnetVersions) {
             $latestAvailableDotnetVersion | Should -BeGreaterOrEqual $version
         }
     }
@@ -29,21 +32,31 @@ Describe "prerequisites" {
     }
 }
 
-Describe "dotnet versions" {
-    # TODO: peek at code above for better version
-    $dotnetMajorVersion = '6'
-    
-    It "dotnet sdk should be up-to-date" {
+Describe "dotnet sdk versions" {
+    It "should be up-to-date" {
         # dotnet sdk check checks for dotnet sdk and runtimes. Should be "Up to date" for Major dotnet version (other versions don't matter)
-        (dotnet sdk check) -match "$dotnetMajorVersion(\.\d+)+  "
+        $dotnetMajorVersions = "$($requiredDotnetVersions.Major)"
+        (dotnet sdk check) -match "[$dotnetMajorVersions](\.\d+)+\s\s+"
         | Where-Object {$_ -notmatch "Up to date"}
         | Should -BeNullOrEmpty
     }
-    It "dotnet packages should be up-to-date" {
-        (dotnet list package --outdated) -match "^   >" | Should -BeNullOrEmpty
-    }
 }
 
-AfterAll {
+Describe "dotnet package versions"{
+
+    foreach ($package in @((dotnet list package --outdated) -match "^   >")) {
+        It "in main project should be up to date" -TestCases @{'package' = $package} {
+            $package | Should -BeNullOrEmpty
+        }
+    }
+
+    Push-Location ./explainpowershell.analysisservice.tests
+    foreach ($package in @((dotnet list package --outdated) -match "^   >")) {
+        It "in testproject should be up to date" -TestCases @{'package' = $package} {
+            $package | Should -BeNullOrEmpty
+        }
+    }
     Pop-Location
 }
+
+Pop-Location
