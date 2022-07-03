d21f4758235efb8b6d059e817aeed1f1dfc304cf
joskw@live.com <joskw@live.com>
2022-07-03 13:19:14 +0000

    add choice for dotnet sdk version

--- tools/bump_versions.ps1
+++ tools/bump_versions.ps1
@@ -12,8 +12,12 @@ $latestAvailableAzuriteVersionTag = $dockerAzuriteImageTagList.tags
 $latestAvailableAzuriteVersionString = $latestAvailableAzuriteVersionTag.Maximum.ToString()
 $dockerComposeFile = "$PSScriptRoot/../.devcontainer/docker-compose.yml"
 
-if (-not (Get-Module -ListAvailable powershell-yaml)) {
-    Install-Module powershell-yam -Force
+$requiredModules = 'Microsoft.PowerShell.ConsoleGuiTools', 'powershell-yaml'
+
+foreach ($module in $requiredModules) {
+    if (-not (Get-Module -ListAvailable $module)) {
+        Install-Module -Name $module -Force
+    }
 }
 
 $compose = Get-Content -Path $dockerComposeFile | ConvertFrom-Yaml
@@ -27,7 +31,7 @@ $compose
 ## Update to latest Dotnet sdk
 
 $availableDotnetSdkDockerImages = (Invoke-RestMethod https://mcr.microsoft.com/v2/dotnet/sdk/tags/list).tags -match '0-bullseye-slim-amd64$'
-$latestDotnetSdkImage = $availableDotnetSdkDockerImages | Select-Object -Last 1
+$latestDotnetSdkImage = $availableDotnetSdkDockerImages | Select-Object -Last 10 | Out-ConsoleGridView -Title "Pick a DotNet SDK image version to migrate to:" -OutputMode Single
 $dotNetLatestVersion = ([version]($latestDotnetSdkImage.split('-')[0]))
 $dockerFilePath = "$PSScriptRoot/../.devcontainer/Dockerfile"
 
@@ -39,7 +43,7 @@ for ($i = 0; $i -lt $dockerFile.Length; $i++) {
 }
 $dockerFile | Out-File -FilePath $dockerFilePath -Force
 
-Get-ChildItem -Path $PSScriptRoot/.. *.csproj -Recurse -Depth 2| ForEach-Object {
+Get-ChildItem -Path $PSScriptRoot/.. *.csproj -Recurse -Depth 2 | ForEach-Object {
     $file = $_
     $xml = [xml](Get-Content $file)
     $xml.Project.PropertyGroup.TargetFramework = "net$($dotNetLatestVersion.Major).$($dotNetLatestVersion.Minor)"
@@ -68,9 +72,8 @@ $ghDeployAction
 | Set-Content -Path $ghActionsDeployAppFilePath -Force
 
 ## Update packages
-Push-Location $PSScriptRoot/..
-
-$outdated = dotnet list package --outdated
+$outdated = dotnet list "$PSScriptRoot/../explainpowershell.sln" package --outdated
+$outdated+= dotnet list "$PSScriptRoot/../explainpowershell.analysisservice.tests/explainpowershell.analysisservice.tests.csproj" package --outdated
 
 $targetVersions = $outdated | Select-String '> ' -Raw | ForEach-Object {
     [pscustomobject]@{
@@ -79,18 +82,21 @@ $targetVersions = $outdated | Select-String '> ' -Raw | ForEach-Object {
     }
 }
 
-Get-ChildItem *.csproj -Recurse -Depth 2| ForEach-Object {
+Get-ChildItem -Path $PSScriptRoot/.. *.csproj -Recurse -Depth 2 | ForEach-Object {
     $file = $_
     $xml = [xml](Get-Content $file)
 
     for ($i = 0; $i -lt $xml.project.ItemGroup.PackageReference.Length; $i++) {
         $package = @($xml.project.ItemGroup.PackageReference)[$i]
         if ($package.Include -in $targetVersions.PackageName) {
-            $package.Version = ( $targetVersions | Where-Object PackageName -EQ $package.Include ).LatestVersion.ToString()
+            $latest = ( $targetVersions | Where-Object PackageName -EQ $package.Include ).LatestVersion
+            if ($latest) {
+                $package.Version = $latest.ToString()
+            }
         }
     }
 
     $xml.Save( $file.FullName )
 }
 
-Pop-Location
+Write-Host -foregroundcolor Magenta "All done. Usually it is a good idea to REBUILD the dev containers now."
