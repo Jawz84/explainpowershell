be8f39b024f33082e3b758cb9868da7ab74f2ecc
Jos Koelewijn <j.koelewijn@rubicon.nl>
2025-12-13 12:32:19 +0100

    Add in-memory help repository seeding for unit tests

---
+++ explainpowershell.analysisservice.tests/helpers/TestHelpData.cs
@@ -0,0 +1,41 @@
+using explainpowershell.models;
+
+namespace ExplainPowershell.SyntaxAnalyzer.Tests
+{
+    internal static class TestHelpData
+    {
+        public static void SeedAboutTopics(InMemoryHelpRepository repository)
+        {
+            repository.AddHelpEntity(new HelpEntity
+            {
+                CommandName = "about_Classes",
+                DocumentationLink = "https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_Classes"
+            });
+
+            repository.AddHelpEntity(new HelpEntity
+            {
+                CommandName = "about_Foreach",
+                DocumentationLink = "https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_Foreach"
+            });
+
+            repository.AddHelpEntity(new HelpEntity
+            {
+                CommandName = "about_For",
+                DocumentationLink = "https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_For"
+            });
+
+            repository.AddHelpEntity(new HelpEntity
+            {
+                CommandName = "about_Remote_Variables",
+                DocumentationLink = "https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_Remote_Variables",
+                RelatedLinks = "https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_Remote_Variables"
+            });
+
+            repository.AddHelpEntity(new HelpEntity
+            {
+                CommandName = "about_Scopes",
+                DocumentationLink = "https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_Scopes"
+            });
+        }
+    }
+}
--- explainpowershell.analysisservice.tests/tests/AstVisitorExplainer_command.tests.cs
+++ explainpowershell.analysisservice.tests/tests/AstVisitorExplainer_command.tests.cs
@@ -6,8 +6,6 @@ using System.Management.Automation;
 using System.Management.Automation.Language;
 using System.Text.Json;
 using System.Threading.Tasks;
-
-using Azure.Data.Tables;
 using explainpowershell.models;
 using NUnit.Framework;
 
@@ -21,10 +19,7 @@ namespace ExplainPowershell.SyntaxAnalyzer.Tests
         public void Setup()
         {
             var mockILogger = new LoggerDouble<LogEntry>();
-            var tableClient = new TableClient(
-                "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;", 
-                "HelpData");
-            var helpRepository = new ExplainPowershell.SyntaxAnalyzer.Repositories.TableStorageHelpRepository(tableClient);
+            var helpRepository = new InMemoryHelpRepository();
 
             explainer = new(
                 extentText: string.Empty,
